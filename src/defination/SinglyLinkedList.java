4e55aeae1d8ffd6671318a4a17e081dbbb92cc0e
Pranjal250 <pranjal.jain_cs18@gla.ac.in>
2020-02-24 19:42:27 +0530

    this method removeNode after the given node

--- src/defination/SinglyLinkedList.java
+++ src/defination/SinglyLinkedList.java
@@ -55,6 +55,15 @@ public class SinglyLinkedList<E> implements SinglyADT<E> {
         }
     }
 
+    private void removeAfter(Node<E> node) {
+        if (node.next == null) {
+            throw new IndexOutOfBoundsException(Integer.toString(size));
+        } else {
+            node.next = node.next.getNext();
+            size--;
+        }
+    }
+
     @Override
     public void remove() {
 
