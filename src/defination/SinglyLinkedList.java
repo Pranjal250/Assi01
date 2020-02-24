ec7d472f8e65249d23c1cca40d76400f6bddcc41
Pranjal250 <pranjal.jain_cs18@gla.ac.in>
2020-02-24 19:21:54 +0530

    this method give the node

--- src/defination/SinglyLinkedList.java
+++ src/defination/SinglyLinkedList.java
@@ -3,6 +3,12 @@ package defination;
 import adt.SinglyADT;
 
 public class SinglyLinkedList<E> implements SinglyADT<E> {
+    private int size = 0;
+    private Node<E> head = null;
+
+    public int getSize() {
+        return size;
+    }
 
     @Override
     public void add(E item) {
@@ -27,5 +33,28 @@ public class SinglyLinkedList<E> implements SinglyADT<E> {
     @Override
     public void print() {
 
+    }
+
+    private static class Node<E> implements Comparable<E> {
+        private E data;
+        private Node<E> next;
+
+        public Node(E data, Node<E> next) {
+            this.data = data;
+            this.next = next;
+        }
+
+        public E getData() {
+            return data;
+        }
+
+        public Node<E> getNext() {
+            return next;
+        }
+
+        @Override
+        public int compareTo(E o) {
+            return 0;
+        }
     }
 }
